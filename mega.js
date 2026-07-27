81f5c690ffa0c92603ac272d1607876e1450b7b9
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-07-27 17:22:18 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ const mega = require('megajs');
 
 const auth = {
     email: process.env.MEGA_EMAIL,
-    password: process.env.MEGA_PASSWORD
+    password: process.env.MEGA_PASSWORD,
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
